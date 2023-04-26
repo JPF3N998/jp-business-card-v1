3782e0743c8f27d599928ad5d9d99b5b29390d39
JPFeng <jpfeng-dev@proton.me>
2023-04-25 22:16:32 -0600

    Added Astro + GitHub Pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://JPF3N998.github.io',
+  base: '/jp-business-card-v1',
+});
