bbc1e68829a43cab2e81141403bc8d683c3e559a
Jose Pablo Feng <jpfeng-dev@proton.me>
2023-04-27 12:58:14 -0600

    Deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://JPF3N998.github.io',
-  base: '/jp-business-card-v1',
 });
