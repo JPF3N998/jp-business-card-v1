a815c9c38738fea2859b0dc0570af3d393f36667
Jose Pablo Feng <jpfeng-dev@proton.me>
2023-04-27 12:44:20 -0600

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://JPF3N998.github.io',
-  base: '/jp-business-card-v1',
-});
+export default defineConfig({});
